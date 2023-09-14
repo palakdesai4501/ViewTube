c66385cab3dc5d2b630fc4ae28b8b39ced470921
Palak <palak.desai4501@gmail.com>
2023-09-14 14:43:08 +0530

    Feature: Imported Five Components into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import SearchFeed from './components/SearchFeed';
 
 const App = () =>
 (
@@ -10,7 +14,7 @@ const App = () =>
       <Routes >
         <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetai />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
