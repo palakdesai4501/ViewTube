b0a893cdf888ee0b0e15baf14bfbe751098c7c25
Palak <palak.desai4501@gmail.com>
2023-09-14 14:49:41 +0530

    Feature: Added index.js file in components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
-import Navbar from './components/Navbar';
-import Feed from './components/Feed';
-import VideoDetail from './components/VideoDetail';
-import SearchFeed from './components/SearchFeed';
+import { Navbar, Feed, VideoDetail, ChannelDetail, ChannelDetail, SearchFeed } from './components';
+
 
 const App = () =>
 (
@@ -14,7 +12,7 @@ const App = () =>
       <Routes >
         <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetai />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
