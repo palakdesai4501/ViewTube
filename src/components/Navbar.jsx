84a571fd719509ce79a906775361da6ae8b8dd14
Palak <palak.desai4501@gmail.com>
2023-09-15 14:22:46 +0530

    Feature: Added logo image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { logo } from '../utils/constants'
@@ -10,9 +9,11 @@ const Navbar = () => (
     p={2}
     sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}
     >
-      
+      <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
     </Stack>
-  )
+  );
 
 
 export default Navbar
