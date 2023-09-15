10133b6d992bbbaeda4dcceaf864956019922703
Palak <palak.desai4501@gmail.com>
2023-09-15 14:08:31 +0530

    Feature: Imported Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { logo } from '../utils/constants'
 
 const Navbar = () => {
   return (
