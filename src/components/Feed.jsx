6c0cd93ffdd57e8cd679382220c46f058f4819d1
Palak <palak.desai4501@gmail.com>
2023-09-18 23:09:47 +0530

    Feature: Added useState Hook For videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,9 +7,11 @@ import { fetchFromAPI } from '../utils/fetchFromAPI'
 const Feed = () => {
 
   const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    fetchFromAPI(`search?part=snippet&q=
+    ${selectedCategory}`).then((data) => setVideos(data.items));
   },[selectedCategory]);
   
   return (
@@ -36,7 +38,7 @@ const Feed = () => {
           {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
       </Box>
-      <Videos videos={[]} />
+      <Videos videos={videos} />
     </Stack>
   )
 }
