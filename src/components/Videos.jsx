ea91aa9bd7debb786be66a6923570bddde357eeb
Palak <palak.desai4501@gmail.com>
2023-09-19 23:46:24 +0530

    Feature: Added VideoCard and ChannelCard Component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -7,7 +7,12 @@ const Videos = ({ videos }) => {
     flexWrap="wrap" 
     justifyContent="start" 
     gap={2}>
-      
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
     </Stack>
   )
 }
