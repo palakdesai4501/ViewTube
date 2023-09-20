4a657fec36ca715d8890f300d61e7b663485b5af
Palak <palak.desai4501@gmail.com>
2023-09-20 12:43:05 +0530

    Feature: Added Cards

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -16,29 +16,21 @@ const Feed = () => {
   
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{
-        height: { sx: 'auto', md: '92vh' },
-        borderRight: '1px solid #3d3d3d',
-        px: { sx: 0, md: 2 }
-      }}>
-        <SideBar selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}/>
-        <Typography
-          className='copyright'
-          variant='body2'
-          sx={{ mt: 1.5, color: '#fff' }}>
-          Copyright 2023 Palak Desai
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
         </Typography>
       </Box>
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
-        <Typography variant='h4'
-          fontWeight="bold" mb={2} sx={{
-            color: 'white'
-          }}>
-          {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
+
+        <Videos videos={videos} />
       </Box>
-      <Videos videos={videos} />
     </Stack>
   )
 }
