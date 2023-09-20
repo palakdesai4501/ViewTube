210fc69066b1b07c76f9e5e100642a64d0dd23a9
Palak <palak.desai4501@gmail.com>
2023-09-20 11:35:42 +0530

    Feature: Imported Some Useful Data in VideoCard Component

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 const VideoCard = () => {
   return (
