d4084cf2a95049578bea0900ce0ae861c25caf88
Palak <palak.desai4501@gmail.com>
2023-09-21 12:58:16 +0530

    Feature: Added marginTop

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -12,7 +12,8 @@ const ChannelCard = ({ channelDetail }) => (
       alignItems: 'center',
       width: { xs: '356px', md: '320px'},
       height: '326px',
-      margin: 'auto'
+      margin: 'auto',
+      marginTop: '-93px'
     }}>
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff'}}>
