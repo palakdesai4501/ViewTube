c081e4c58e9ad5b848a6c2132be3e78f66922717
Palak <palak.desai4501@gmail.com>
2023-09-26 13:34:20 +0530

    Feature: Added ReactPlayer Component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -7,12 +7,20 @@ import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
+  const [videoDeatil, setVideoDeatil] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+    .then((data) => setVideoDeatil(data.items[0]));
+  }, [id])
+  
   return (
     <Box minHeight="95vh">
-      <Stack direction={{ xs: 'column', md: 'row'}}>
+      <Stack direction={{ xs: 'column', md: 'row' }}>
         <Box flex={1}>
-          <Box sx={{ width: '100%', position: 'sticky', top: '86px'}}>
-                 <ReactPlayer/>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}/>
           </Box>
         </Box>
       </Stack>
