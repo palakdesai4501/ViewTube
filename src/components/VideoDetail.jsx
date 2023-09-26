031eb70e701d2f1511325022a8e9ed75e4898822
Palak <palak.desai4501@gmail.com>
2023-09-26 18:29:42 +0530

    Feature: Added Link And Typography Component And Added Logic If videoDetail Can't load

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -7,20 +7,36 @@ import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
-  const [videoDeatil, setVideoDeatil] = useState(null);
+  const [videoDetail, setVideoDetail] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-    .then((data) => setVideoDeatil(data.items[0]));
+    .then((data) => setVideoDetail(data.items[0]));
   }, [id])
+
+  if (!videoDetail?.snippet) return 'Loading...';
+
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount} } = videoDetail;
   
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: 'column', md: 'row' }}>
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}/>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} 
+            className="react-player" controls/>
+            <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
+             {title}
+            </Typography> 
+            <Stack direction="row" justifyContent="space-between" sx={{ color: '#fff' }} py={1} px={2}>
+              <Link to ={`/channel/${channelId}`}>
+                <Typography>
+                  {channelId}
+                </Typography>
+              </Link>
+
+            </Stack>
           </Box>
         </Box>
       </Stack>
