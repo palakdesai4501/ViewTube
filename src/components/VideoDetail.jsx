71308e3006ee6cd51756aab5105c605502b411d5
Palak <palak.desai4501@gmail.com>
2023-09-26 13:29:56 +0530

    Feature: Started Creating Layout fro VideoDetail Component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Box, Stack, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   return (
-    <div>
-      VideoDetail
-    </div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px'}}>
+                 <ReactPlayer/>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
