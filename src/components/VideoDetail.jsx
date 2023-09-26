b937decd0a1560d8a5d640b3fedc1ef71e54e9d3
Palak <palak.desai4501@gmail.com>
2023-09-26 18:43:43 +0530

    Feature: Added Views

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -31,11 +31,16 @@ const VideoDetail = () => {
             </Typography> 
             <Stack direction="row" justifyContent="space-between" sx={{ color: '#fff' }} py={1} px={2}>
               <Link to ={`/channel/${channelId}`}>
-                <Typography>
-                  {channelId}
+                <Typography variant={{ sm: 'subtitle1', md: 'h6'}} color="#fff">
+                  {channelTitle}
+                  <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml:'5px'}} />
                 </Typography>
               </Link>
-
+              <Stack>
+                <Typography variant='body1' sx={{ opacity: 0.7 }}>
+                  {viewCount} views
+                </Typography>
+              </Stack>
             </Stack>
           </Box>
         </Box>
